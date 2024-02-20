bfdace166169e39f6c66dac02f60293a34ce6b90
Muhriddin Valiyev <muhriddinvaliyev177@gmail.com>
2024-02-20 16:05:19 +0500

    hello

--- app/src/main/java/com/example/infoapp/InfoActivity.java
+++ app/src/main/java/com/example/infoapp/InfoActivity.java
@@ -3,7 +3,11 @@ package com.example.infoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -12,5 +16,21 @@ public class InfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         findViewById(R.id.back_info).setOnClickListener(v -> finish());
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            Window window = this.getWindow();
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(getResources().getColor(R.color.black));
+//        }
+//
+//        // Hide system UI like status bar and navigation bar (optional)
+//        hideSystemUI();
     }
+
+//    private void hideSystemUI() {
+//        View decorView = getWindow().getDecorView();
+//        // Hide the status bar.
+//        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+//        decorView.setSystemUiVisibility(uiOptions);
+//    }
 }
